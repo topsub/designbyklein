e12ee2ef3188388da283c6ab6d6345d3e73a2a93
rhino lights <topsub@gmail.com>
2024-04-18 12:17:40 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Charlotte eCommerce Digital Marketing Agency | Designbyklein</title>
+        <meta name="keywords" content="web development, web design, cs-cart, content management systems, web applications" />
+        <meta name="author" content="Josh Klein - Designbyklein.com" />
+        <meta name="description" content="Josh Klein is a freelance web developer with over 15 years of experience in content management systems, cs-cart, web application and database architecture.">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="manifest" href="site.webmanifest">
+        <link rel="apple-touch-icon" href="icon.png">
+        <!-- Place favicon.ico in the root directory -->
+        <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        <div class="container">
+            <div class="row mt-5">
+                <div class="col-lg">
+                    <img src="/img/logo.jpg">
+                </div>
+                <div class="col-lg">
+                    <h2 class="text-right">Interactive  <span class="text-orange">innovation</span> <span class="text-red">defined</span></h2>
+                </div>
+            </div>
+            <div class="row mb-5">
+                <div class="col-lg">
+                    <img src="/img/dbk-text.png">
+                </div>
+            </div>
+            <div class="row mb-5">
+                <div class="col-lg"><p><span class="text-orange">Designbyklein</span> is an established user experience design firm located in Charlotte, NC, fully equipped to offer a full host of consulting, design, and development services to companies big and small. Our team has grown and so must our site. We're currently developing our new portfolio site to better showcase our new work. </p>
+                    <p>In the mean time if you need work done? Feel free to contact us at <span class="text-orange">704.779.4239</span> or email us at <span class="text-orange">info@designbyklein.com.</span></p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg">
+                    <h2 class="clearfix"><span class="text">Contact:</span><span class="hr-line"></span></h2>
+                    <p class="mb-0">Phone: 704.779.4239</p>
+                    <p>Email: info@designbyklein.com</p>
+                </div>
+                <div class="col-lg">
+                    <h2 class="clearfix"><span class="text">Services:</span><span class="hr-line"></span></h2>
+                    <ul>
+                        <li>Consulting</li>
+                        <li>Web Application Development</li>
+                        <li>Database Architecture</li>
+                        <li>CS-Cart Consulting and Setup</li>
+                        <li>Custom Hosting</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg">
+                    <h2 class="clearfix"><span class="text">Our Work:</span><span class="hr-line"></span></h2>
+                </div>
+            </div>
+            <div class="row mb-2">
+                <div class="col-lg"><img class="img-responsive img-border" src="/img/our-work/1095-ez/upload_1_1095-listing.jpg"></div>
+                <div class="col-lg"><img class="img-responsive img-border" src="/img/our-work/commissions-ez/upload_1_commission-listings.jpg"></div>
+            </div>
+            <div class="row mb-2">
+                <div class="col-lg"><img class="img-responsive img-border" src="/img/our-work/bullys-performance/upload_1_listing-bullys.jpg"></div>
+                <div class="col-lg"><img class="img-responsive img-border" src="/img/our-work/mom-4-life/upload_1_listing-mom4life.jpg"></div>
+            </div>
+            <div class="row mb-2">
+                <div class="col-lg"><img class="img-responsive img-border" src="/img/our-work/raptor-svt-parts/upload_1_listing-raptor.jpg"></div>
+                <div class="col-lg"><img class="img-responsive img-border" src="/img/our-work/booty-bean-bag/upload_1_listing.jpg"></div>
+            </div>
+        </div>
+        <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+        <script>
+            window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+            ga('create','UA-4281952-3','auto');ga('send','pageview')
+        </script>
+        <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+    </body>
+</html>
